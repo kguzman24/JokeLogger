cfe38852f5ba889b88cbb63290ccd88a413033cb
Karen Guzman <krngzmn10@gmail.com>
2024-10-10 14:38:45 +0000

    cleaning

--- app.py
+++ app.py
@@ -17,4 +17,8 @@ setup = response['setup']
 punchline = response['punchline']
 
 
-print(id, setup, punchline)
+#print(id, setup, punchline)
+
+Logger = logging.getLogger(__name__)
+logging.basicConfig(filename="joke.log", encoding = "utf-8", level=logging.DEBUG)
+logging.warning('%s:%s:%s', id, setup, punchline)
